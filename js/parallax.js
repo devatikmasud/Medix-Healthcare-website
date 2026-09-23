8f129e05698ed1cf5826d4523c8a361759875857
Atik masud <Atik03072007@gmail.com>
2026-09-23 20:51:53 +0600

    Add files via upload

---
+++ js/parallax.js
@@ -0,0 +1,12 @@
+/*
+    Project: Medix
+    Developed by: Atik Masud
+    Copyright © 2026 Atik Masud
+*/
+
+
+   // ======================================================
+	    // Parallax
+	// ======================================================
+	 $('.info_section , .paralax').parallax("50%", 0.2)
+	
